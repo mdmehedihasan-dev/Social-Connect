8040b758c0b0366ef600dabcee74c5cd8e97f9d6
Md Mehedi Hasan <116740305+DevMehedi-CE@users.noreply.github.com>
2024-04-24 08:38:45 +0600

    profile image upload

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@
 
 const Button = ({className,btnName, onClick}) => {
   return (
-   <button onClick={onClick} className={`${className}  text-gray-600 font-mono font-semibold px-2 py-1 mt-2 rounded-md bg-blue-700 w-full md:w-96`}>{btnName}</button>
+   <button onClick={onClick} className={`${className}  text-white font-mono font-semibold px-2 py-1 mt-2 rounded-md bg-blue-700 w-full md:w-96`}>{btnName}</button>
   )
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,53 +1,54 @@
 /* eslint-disable no-unused-vars */
 import Image from "./Image";
-import {  FaHome, FaMoon, FaSun } from "react-icons/fa";
+import { FaHome, FaMoon, FaSun } from "react-icons/fa";
 import { AiFillMessage } from "react-icons/ai";
-import {  IoMdSettings } from "react-icons/io";
+import { IoMdSettings } from "react-icons/io";
 import { TbLogout2 } from "react-icons/tb";
-import { getAuth, signOut } from "firebase/auth";
+import { getAuth, signOut,updateProfile } from "firebase/auth";
 import { Link, useNavigate, useLocation } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { activeUser } from "../slice/userSlice";
 import { useState, createRef } from "react";
 import { Cropper } from "react-cropper";
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import Modal from '@mui/material/Modal';
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Modal from "@mui/material/Modal";
 import "cropperjs/dist/cropper.css";
 import { toggleTheme } from "../slice/themeSlice";
-const defaultSrc =
-  "https://raw.githubusercontent.com/roadmanfong/react-cropper/master/example/img/child.jpg";
+import { Bars } from "react-loader-spinner";
+import { getStorage, ref, uploadString,getDownloadURL } from "firebase/storage";
+
+// const defaultSrc =
+//   "https://raw.githubusercontent.com/roadmanfong/react-cropper/master/example/img/child.jpg";
 
 const Navbar = () => {
-  const {theme} = useSelector(state=>state.theme)
+  const auth = getAuth();
+  const storage = getStorage();
 
-  // state for modal 
+
+  const { theme } = useSelector((state) => state.theme);
+
+  // state for modal
 
   const [open, setOpen] = useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
 
-
-
-  //  state for crop images 
-  const [image, setImage] = useState(defaultSrc);
+  //  state for crop images
+  const [image, setImage] = useState("");
   const [cropData, setCropData] = useState("#");
   const cropperRef = createRef();
 
+  let [loading, setLoading] = useState(false);
 
   let location = useLocation();
   let dispatch = useDispatch();
   let userInfo = useSelector((state) => state.user.value);
   // console.log(userInfo.value)
-  const auth = getAuth();
   const navigate = useNavigate();
 
   const handleSignOut = () => {
-
-    
-
-
     signOut(auth)
       .then(() => {
         localStorage.removeItem("user");
@@ -59,34 +60,49 @@ const Navbar = () => {
       });
   };
 
-  // function for crop image 
-
+  // function for crop image
 
   const getCropData = () => {
-    if (typeof cropperRef.current?.cropper !== "undefined") {
-      setCropData(cropperRef.current?.cropper.getCroppedCanvas().toDataURL());
-    }
+    // if (typeof cropperRef.current?.cropper !== "undefined") {
+    //   setCropData(cropperRef.current?.cropper.getCroppedCanvas().toDataURL());
+    // }
+    const storageRef = ref(storage, `profile- ${userInfo.uid}`);
+    const message4 = cropperRef.current?.cropper.getCroppedCanvas().toDataURL();
+    uploadString(storageRef, message4, "data_url").then((snapshot) => {
+      console.log("Uploaded a data_url string!");
+      getDownloadURL(storageRef).then((downloadURL) => {
+        console.log('File available at', downloadURL);
+        updateProfile(auth.currentUser, {
+        photoURL:downloadURL
+        }).then(()=>{
+          localStorage.setItem("user",JSON.stringify({...userInfo,photoURL:downloadURL}))
+            dispatch(activeUser({...userInfo,photoURL:downloadURL}))
+        })
+      });
+    });
+
+    setOpen(false);
   };
 
-// style for modal ==========
+  // style for modal ==========
   const style = {
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
     width: 400,
-    bgcolor: 'background.paper',
-    border: '2px solid #000',
+    bgcolor: "background.paper",
+    border: "2px solid #000",
     boxShadow: 24,
     p: 4,
   };
 
   // style end modal ==========
- 
-// function for image upload 
+
+  // function for image upload
   const handleImageUpload = (e) => {
     e.preventDefault();
-    let files = e.target.files ;
+    let files = e.target.files;
     // if (e.dataTransfer) {
     //   files = e.dataTransfer.files;
     // } else if (e.target) {
@@ -98,10 +114,6 @@ const Navbar = () => {
     };
     reader.readAsDataURL(files[0]);
   };
-
-
-
-
 
   return (
     <div className="min-h-screen bg-gray-600 w-36 rounded-r-xl">
@@ -135,17 +147,9 @@ const Navbar = () => {
           <IoMdSettings className="mx-auto icon w-[60%] " />
         </Link>
 
-         
-        <button className="mx-auto" 
-        onClick={()=>dispatch(toggleTheme())}
-        >
-          {
-            theme === "light" ?  <FaMoon /> : <FaSun/>
-          }
-         
+        <button className="mx-auto" onClick={() => dispatch(toggleTheme())}>
+          {theme === "light" ? <FaMoon /> : <FaSun />}
         </button>
-
-
       </div>
 
       <div className="mt-20 text-4xl font-bold text-white cursor-pointer">
@@ -161,41 +165,64 @@ const Navbar = () => {
         aria-describedby="modal-modal-description"
       >
         <Box sx={style}>
-          <Typography id="modal-modal-title" >
-          <div className="box" style={{ width: "50%", float: "right" }}>
-          <h1>Preview</h1>
-          <div
-            className="img-preview"
-            style={{ width: "100%", float: "left", height: "300px" }}
-          />
-        </div>
+          <Typography id="modal-modal-title">
+           {image && 
+            <div className="box" style={{ width: "50%", float: "right" }}>
+            <h1>Preview</h1>
+            <div
+              className="img-preview"
+              style={{ width: "100%", float: "left", height: "300px" }}
+            />
+          </div>
+           }
           </Typography>
-          <Typography id="modal-modal-description" >
-
-          <input onChange={handleImageUpload} type="file" />  
+          <Typography id="modal-modal-description">
+            <input onChange={handleImageUpload} type="file" />
             <h1>Image Upload</h1>
-          <Cropper
-          ref={cropperRef}
-          style={{ height: 300, width: "100%" }}
-          zoomTo={0.5}
-          initialAspectRatio={1}
-          preview=".img-preview"
-          src={image}
-          viewMode={1}
-          minCropBoxHeight={10}
-          minCropBoxWidth={10}
-          background={false}
-          responsive={true}
-          autoCropArea={1}
-          checkOrientation={false} // https://github.com/fengyuanchen/cropperjs/issues/671
-          guides={true}
-        />
-        {/* <Button onClick={getCropData} btnName='Upload' />  */}
-        <button className="px-5 py-1 mt-2 font-bold bg-gray-600 rounded-md text-yellow-50" onClick={getCropData}>Upload</button>
+           {image && 
+            <>
+             <Cropper
+              ref={cropperRef}
+              style={{ height: 300, width: "100%" }}
+              zoomTo={0.5}
+              initialAspectRatio={1}
+              preview=".img-preview"
+              src={image}
+              viewMode={1}
+              minCropBoxHeight={10}
+              minCropBoxWidth={10}
+              background={false}
+              responsive={true}
+              autoCropArea={1}
+              checkOrientation={false} // https://github.com/fengyuanchen/cropperjs/issues/671
+              guides={true}
+            />
+
+            <div>
+              {loading ? (
+                <Bars
+                  height="40"
+                  width="80"
+                  color="#1a56db"
+                  ariaLabel="bars-loading"
+                  wrapperStyle={{}}
+                  wrapperClass=""
+                  visible={true}
+                />
+              ) : (
+                <button
+                  className="px-5 py-1 mt-2 font-bold bg-gray-600 rounded-md text-yellow-50"
+                  onClick={getCropData}
+                >
+                  Upload{" "}
+                </button>
+              )}
+            </div>
+            </>
+           }
           </Typography>
         </Box>
       </Modal>
-        
     </div>
   );
 };
