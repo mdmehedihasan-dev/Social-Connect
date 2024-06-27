c61c867431e7d2daca4dc8861b23fc8ce39ed316
Md Mehedi Hasan <116740305+DevMehedi-CE@users.noreply.github.com>
2024-06-27 11:39:41 +0600

    emoji

--- src/pages/Message.jsx
+++ src/pages/Message.jsx
@@ -8,10 +8,12 @@ import { getDatabase, onValue, push, ref, set } from "firebase/database";
 import moment from "moment";
 import AllMessage from "./AllMessage";
 import { BsSendFill } from "react-icons/bs";
+import EmojiPicker from "emoji-picker-react";
 
 const Message = () => {
   const db = getDatabase();
   const [msg, setMsg] = useState("");
+  const [emojishow, setEmojiShow] = useState(false);
   const [msgList, setMsgList] = useState([]);
 
   let active = useSelector((state) => state.activeChat.value);
@@ -36,6 +38,10 @@ const Message = () => {
     });
   };
 
+  let handleEmoji = (e) => {
+    setMsg(msg + e.emoji);
+  };
+
   useEffect(() => {
     if (active == null) {
       return;
@@ -58,9 +64,8 @@ const Message = () => {
   }, [active?.activeuserid]);
 
   return (
-    <div className="flex justify-between">
-      <div >
-        {/* <MsgFriend /> */}
+    <div className="flex items-center justify-between">
+      <div>
         <AllMessage />
         <MsgGroup />
       </div>
@@ -81,7 +86,7 @@ const Message = () => {
                 item.whosendid == userInfo.uid &&
                 item.whoreceiveid == active.activeuserid ? (
                   <div key={i} className="text-right">
-                    <div className="inline-block px-2 py-1 mb-20 bg-blue-500 border-2 rounded-xl">
+                    <div className="inline-block px-2 py-1 mb-20 bg-green-500 border-2 rounded-xl">
                       <p className="text-lg text-blue-800 ">{item.msg}</p>
                       <p className="dark:text-black">
                         {" "}
@@ -94,7 +99,7 @@ const Message = () => {
                   item.whoreceiveid == userInfo.uid &&
                   item.whosendid == active.activeuserid && (
                     <div key={i} className="text-left">
-                      <div className="inline-block px-2 py-1 bg-green-500 border-2 rounded-xl">
+                      <div className="inline-block px-2 py-1 bg-blue-500 border-2 rounded-xl">
                         <p className="text-lg text-gray-800 ">{item.msg}</p>
                         <p className="dark:text-black">
                           {moment(item.date, "YYYYMMDD h:mm:ss").fromNow()}
@@ -105,11 +110,9 @@ const Message = () => {
                 )
               )}
 
-          
-
             {/* write message ====================== */}
 
-            <div className="absolute rounded-bl-md rounded-br-md  bottom-0  lg:w-[700px] border-t p-3 bg-green-500">
+            <div className="absolute rounded-md bottom-0  lg:w-[700px] border-t p-3 bg-green-500">
               <div className="flex items-center justify-between">
                 <div className="flex items-center w-full gap-x-2">
                   <div className="relative w-full">
@@ -119,6 +122,20 @@ const Message = () => {
                       className="w-full p-3 rounded-xl dark:bg-black"
                       type="text"
                     />
+
+                    {emojishow && (
+                      <div className="absolute -top-[500px] right-5">
+                        <EmojiPicker onEmojiClick={handleEmoji} />
+                      </div>
+                    )}
+                    {/* emoji button ============== */}
+                    <button
+                      onClick={() => setEmojiShow(!emojishow)}
+                      className="absolute p-2 mr-2 text-blue-800 right-5 top-1 rounded-xl"
+                    >
+                      😎
+                    </button>
+                    {/* send button ============== */}
                     <button
                       className="absolute right-0 p-3 text-blue-800 top-1 rounded-xl"
                       onClick={handleChat}
